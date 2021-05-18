c6fd6fbdb40bf0de1ff8fd303644c25375502d26
Hallvardr <alvarobb92@gmail.com>
2021-05-18 11:48:25 +0200

    Delete demo_ew1/sql directory

--- demo_ew1/sql/billing/dve_count.sql
+++
@@ -1,7 +0,0 @@
-set @date_from = CURDATE() - INTERVAL 1 day;
-set @date_to = CURDATE();
-
-SELECT auth_user, DATABASE() as customer, response_key, COUNT(*) AS dv_transactions
-FROM doc_verification
-WHERE reception_time BETWEEN @date_from AND @date_to
-GROUP BY 1,response_key
--- demo_ew1/sql/billing/lsv_count.sql
+++
@@ -1,8 +0,0 @@
-set @date_from = CURDATE() - INTERVAL 1 day;
-set @date_to = CURDATE();
-
-SELECT auth_user, channel, result, COUNT(*) AS total_lsv
-FROM liveness_verification
-WHERE start_time BETWEEN @date_from AND @date_to
-
-GROUP BY auth_user,channel,result
